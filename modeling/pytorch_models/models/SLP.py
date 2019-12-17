96548f5a06e9acc05bd2409576ae4138edef6107
willsterJ <wj419@nyu.edu>
2019-12-17 16:06:13 -0500

    pytorch models

--- modeling/pytorch_models/models/SLP.py
+++ modeling/pytorch_models/models/SLP.py
@@ -12,16 +12,16 @@ class SLP(nn.Module):
         hidden_size = 100
         output_size = Config.output_size
 
-        self.word_embeddings = nn.Embedding(vocab_size, embedding_size)  # Initializing the look-up table.
-        self.word_embeddings.weight = nn.Parameter(word_embeddings, requires_grad=False)  # Assigning the look-up table to the pre-trained GloVe
-
+        self.word_embeddings = nn.Embedding(vocab_size, embedding_size)
+        self.word_embeddings.weight = nn.Parameter(word_embeddings, requires_grad=False)
+        
         self.fc1 = nn.Linear(embedding_size, hidden_size)
         self.Tanh = nn.Tanh()
         self.fc2 = nn.Linear(hidden_size, output_size)
 
     def forward(self, x):
-        x = self.word_embeddings(x)  # embedded input of shape = (batch_size, num_sequences,  embedding_length)
-        x = x.permute(1, 0, 2)  # input.size() = (num_sequences, batch_size, embedding_length)
+        x = self.word_embeddings(x)
+        x = x.permute(1, 0, 2)
         x = self.fc1(x)
         x = self.Tanh(x)
         x = self.fc2(x[-1])
